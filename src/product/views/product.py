d1a9ced28d8f676a6f327e18d0c763d569a70787
nipulkm <nipulkm@gmail.com>
2023-03-16 23:44:01 +0600

    Product Create Implemented

--- src/product/views/product.py
+++ src/product/views/product.py
@@ -1,8 +1,14 @@
 import datetime
+import json
+
+from django.views.decorators.csrf import csrf_exempt
 from django.views import generic
 from django.views.generic.list import ListView
+from django.views import View
+from django.http import JsonResponse
+from django.http import HttpResponse
 
-from product.models import Product, Variant
+from product.models import Product, ProductVariant, ProductVariantPrice, Variant
 
 
 class CreateProductView(generic.TemplateView):
@@ -48,3 +54,45 @@ class ListProductView(ListView):
         context = super().get_context_data(**kwargs)
         context['variants'] = Variant.objects.prefetch_related('productvariant_set')
         return context
+
+
+
+class ProductView(View):
+    @csrf_exempt
+    def post(self, request):
+        data = json.loads(request.body.decode('utf-8'))
+        # try:
+        product = Product(
+            title=data.get('title'), sku=data.get('sku'), description=data.get('description')
+        )
+        product.save()
+
+        product_variant_data = []
+        for item in data.get('product_variant', []):
+            for tag in item.get('tags'):
+                product_variant_data.append(ProductVariant(variant_title=tag, variant_id=item.get('option'), product=product))
+        if len(product_variant_data):
+            ProductVariant.objects.bulk_create(product_variant_data)
+
+        product_variant_prices_data = []
+        for item in data.get('product_variant_prices', []):
+            product_variant_titles = item.get('title').split('/') if item.get('title') \
+                else []
+            product_variant_one = ProductVariant.objects.filter(variant_title=product_variant_titles[0]).first() if len(product_variant_titles) and ProductVariant.objects.filter(variant_title=product_variant_titles[0]).exists() else None
+            product_variant_two = ProductVariant.objects.filter(variant_title=product_variant_titles[0]).first() if len(product_variant_titles) > 1 and ProductVariant.objects.filter(variant_title=product_variant_titles[1]).exists() else None
+            product_variant_three = ProductVariant.objects.filter(variant_title=product_variant_titles[0]).first() if len(product_variant_titles) > 2 and ProductVariant.objects.filter(variant_title=product_variant_titles[2]).exists() else None
+            product_variant_prices_data.append(ProductVariantPrice(
+                product_variant_one=product_variant_one,
+                product_variant_two=product_variant_two,
+                product_variant_three=product_variant_three,
+                price=item.get('price'),
+                stock=item.get('stock'),
+                product=product
+            ))
+        if len(product_variant_prices_data):
+            ProductVariantPrice.objects.bulk_create(product_variant_prices_data)
+
+        return JsonResponse({'message': 'Created!'})
+
+        # except Exception as e:
+        #     return JsonResponse({'error': str(e)})
--- src/templates/assets/js/components/product/CreateProduct.vue
+++ src/templates/assets/js/components/product/CreateProduct.vue
@@ -189,8 +189,10 @@ export default {
         product_variant_prices: this.product_variant_prices
       }
 
+    axios.defaults.xsrfCookieName = 'csrftoken'
+  axios.defaults.xsrfHeaderName = "X-CSRFTOKEN"
 
-      axios.post('/product', product).then(response => {
+      axios.post('/product/product-create/', product).then(response => {
         console.log(response.data);
       }).catch(error => {
         console.log(error);
